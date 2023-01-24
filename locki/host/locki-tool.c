6b25a92ce833f839cc0ca5d94c56ceb3df07e298
Joakim Bech <joakim.bech@linaro.org>
2023-01-24 15:26:48 +0100

    locki: implement template code for 'measure'
    Note, that running this here doesn't support unauthenticated measure,
    likewise it will not really be possible to do measurements here as it is
    right now, since the TA is created when invoking this. I.e., there are
    no users or anything stored. For this to work, there either needs to be
    debug functions (initialize and terminate) available or the TA has to be
    configured with the keep alive property. We will address this in later
    commits.
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- locki/host/locki-tool.c
+++ locki/host/locki-tool.c
@@ -24,7 +24,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
 #include <argp.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -57,7 +56,7 @@ int print_help(void)
 /*******************************************************************************
  * Subcommand: User
  ******************************************************************************/
-static char doc[] = "'locki user', is used to create and manage users.";
+static char doc_user[] = "'locki user', is used to create and manage users.";
 static char args_doc[] = "ARG1 ARG2";
 
 static struct argp_option options_user[] = {
@@ -108,11 +107,12 @@ static struct argp argp_user = {
 	options_user,
 	parse_opt_user,
 	args_doc,
-	doc
+	doc_user
 };
 
 int user_main(int argc, char *argv[])
 {
+	int res = -1;
 	struct arguments_user arg;
 
 	arg.username = NULL;
@@ -121,7 +121,6 @@ int user_main(int argc, char *argv[])
 	arg.flags = 0;
 	arg.verbose = false;
 
-	argp_parse(&argp_user, argc, argv, 0, NULL, NULL);
 	argp_parse(&argp_user, argc, argv, 0, 0, &arg);
 
 	verbose = arg.verbose;
@@ -129,20 +128,104 @@ int user_main(int argc, char *argv[])
 	if (arg.username && arg.password) {
 		printf("Creating user: %s\n", arg.username);
 		if (arg.salt) {
-			create_user(arg.username, strlen(arg.username),
-				    arg.password, strlen(arg.password),
-				    arg.salt, sizeof(arg.salt),
-				    arg.flags);
+			res = create_user(arg.username, strlen(arg.username),
+					  arg.password, strlen(arg.password),
+					  arg.salt, sizeof(arg.salt),
+					  arg.flags);
 		} else {
-			create_user(arg.username, strlen(arg.username),
-				    arg.password, strlen(arg.password),
-				    NULL, 0,
-				    arg.flags);
+			res = create_user(arg.username, strlen(arg.username),
+					  arg.password, strlen(arg.password),
+					  NULL, 0,
+					  arg.flags);
 		}
 		debug_dump_users();
 	}
 
+	return res;
+}
+
+/*******************************************************************************
+ * Subcommand: Measure
+ ******************************************************************************/
+static char doc_measure[] = "'locki measure', is used to create and manage users.";
+
+static struct argp_option options_measure[] = {
+	{ "data",      'd', "DATA",      0,  "Data to measure" },
+	{ "password",  'p', "VALUE",     0,  "Authentication password" },
+	{ "reg",       'r', "ID",        0,  "Register ID" },
+	{ "user",      'u', "USERNAME",  0,  "The name of the user" },
+	{ "verbose",   'v', 0,           0,  "Verbose output" },
+	{ 0 }
+};
+
+struct arguments_measure {
+	char *username;
+	char *password;
+	char *reg;
+	char *data;
+	bool verbose;
+};
+
+static error_t parse_opt_measure(int key, char *arg, struct argp_state *state)
+{
+	struct arguments_measure *arguments =
+		(struct arguments_measure *)state->input;
+
+	switch (key) {
+	case 'd':
+		arguments->data = arg;
+		break;
+	case 'p':
+		arguments->password = arg;
+		break;
+	case 'r':
+		arguments->reg = arg;
+		break;
+	case 'u':
+		arguments->username = arg;
+		break;
+	case 'v':
+		arguments->verbose = true;
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
 	return 0;
+}
+
+static struct argp argp_measure = {
+	options_measure,
+	parse_opt_measure,
+	args_doc,
+	doc_measure
+};
+
+int measure_main(int argc, char *argv[])
+{
+	int res = -1;
+	struct arguments_measure arg;
+
+	arg.username = NULL;
+	arg.password = NULL;
+	arg.reg = NULL;
+	arg.data = NULL;
+	arg.verbose = false;
+
+	argp_parse(&argp_measure, argc, argv, 0, 0, &arg);
+
+	verbose = arg.verbose;
+
+
+	if (arg.username && arg.password) {
+		res = measure(arg.username, strlen(arg.username),
+			      NULL, 0,
+			      (uint8_t *)arg.reg, strlen(arg.reg),
+			      (uint8_t *)arg.data, strlen(arg.data));
+		printf("Measure: %s\n", arg.username);
+		debug_dump_users();
+	}
+
+	return res;
 }
 
 /*******************************************************************************
@@ -151,11 +234,11 @@ int user_main(int argc, char *argv[])
 int main(int argc, char *argv[])
 {
 	int res = -1;
+
 	if (argc > 1 && (strcmp(argv[1], "user") == 0)) {
-		res = user_main(argc, argv);
-	}
-	else if (argc > 1 && (strcmp(argv[1], "measure") == 0)) {
-		printf("TODO: add measure\n");
+		res = user_main(argc-1, &argv[1]);
+	} else if (argc > 1 && (strcmp(argv[1], "measure") == 0)) {
+		res = measure_main(argc-1, &argv[1]);
 	} else {
 		print_help();
 	}
